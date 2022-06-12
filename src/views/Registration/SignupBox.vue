ace82d7b8022a68f55f0ef82e1117ed3ab886601
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-06-12 17:39:06 +0100

    signup page responsiveness completed

--- src/views/Registration/SignupBox.vue
+++ src/views/Registration/SignupBox.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="signup grid-center">
     <div class="signup-box">
-      <h3>Create Account</h3>
+      <router-link to="/">
+        <i class="fas fa-times"></i>
+      </router-link>
+      <h4>Create Account</h4>
       <form action="#">
         <div class="form-line">
           <div class="form-item">
@@ -19,7 +22,13 @@
             <label>Email Address</label>
           </div>
           <div class="form-item">
-            <input type="password" required />
+            <input
+              :type="showPassword ? 'text' : 'password'"
+              required
+              minlength="8"
+              @keyup="validatePassword"
+              v-model="password"
+            />
 
             <label>Password</label>
             <i
@@ -28,11 +37,45 @@
               @click="togglePassword"
             ></i>
             <i v-else class="fas fa-eye-slash" @click="togglePassword"></i>
+            <span
+              v-show="validator"
+              class="strength-validation"
+              :class="validator ? errorMessage : ''"
+              >Password strength is {{ errorMessage }}</span
+            >
           </div>
         </div>
-        <div class="form-item">
-          <input type="tel" required />
-          <label>Phone number(optional)</label>
+        <div class="form-line">
+          <div class="form-item">
+            <input
+              :type="confirmPassword ? 'text' : 'password'"
+              minlength="8"
+              required
+              v-model="password2"
+              @keyup="validatePassword2"
+            />
+            <label>Confirm Password</label>
+            <i
+              v-if="confirmPassword"
+              class="fas fa-eye"
+              @click="toggleConfirmPassword"
+            ></i>
+            <i
+              v-else
+              class="fas fa-eye-slash"
+              @click="toggleConfirmPassword"
+            ></i>
+            <span
+              class="strength-validation"
+              :class="validator2 ? 'strong' : 'weak'"
+            >
+              {{ confirmPasswordValidator }}
+            </span>
+          </div>
+          <div class="form-item">
+            <input type="tel" required />
+            <label>Phone number</label>
+          </div>
         </div>
         <div class="checkbox">
           <input type="checkbox" />
@@ -50,7 +93,11 @@
         </div>
         <div class="action-buttons">
           <action-button btnvalue="CREATE ACCOUNT" />
-          <p><hr><span>OR</span><hr></p>
+          <div class="or">
+            <hr />
+            <span>OR</span>
+            <hr />
+          </div>
           <router-link to="/"
             ><action-button class="gmail-btn" btnvalue="REGISTER WITH GMAIL"
               ><img
@@ -59,11 +106,10 @@
           ></router-link>
         </div>
         <div class="form-footer">
-            <p>Already have an account?</p>
-            <router-link to="/login">LOGIN</router-link>
+          <p>Already have an account?</p>
+          <router-link to="/login">LOGIN</router-link>
         </div>
       </form>
-    
     </div>
   </section>
 </template>
@@ -75,31 +121,150 @@ export default {
   name: "SignUpBox",
   data() {
     return {
+      password: "",
+      password2: "",
       showPassword: false,
+      confirmPassword: false,
+      passwordValidator: false,
+      confirmPasswordValidator: "",
+      errorMessage: "",
+      validator: false,
+      validator2: false,
     };
   },
   methods: {
     togglePassword() {
       this.showPassword = !this.showPassword;
     },
+    toggleConfirmPassword() {
+      this.confirmPassword = !this.confirmPassword;
+    },
+    validatePassword() {
+      if (this.password.length === 0) {
+        this.validator = false;
+      } else {
+        this.validator = true;
+        if (this.password.length >= 8) {
+          this.errorMessage = "strong";
+        } else if (this.password.length >= 4 && this.password.length < 8) {
+          this.errorMessage = "medium";
+        } else if (this.password.length < 4) {
+          this.errorMessage = "weak";
+        }
+      }
+    },
+    validatePassword2() {
+      if (this.password2.length != 0) {
+        if (this.password2 === this.password) {
+          this.confirmPasswordValidator = "Passwords match";
+          this.validator2 = true;
+        } else {
+          this.confirmPasswordValidator = "Passwords do not match";
+          this.validator2 = false;
+        }
+      } else {
+        this.confirmPasswordValidator = "";
+        this.validator2 = false;
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
-.form-footer {
-  text-align: center;
+.grid-center {
+  display: grid;
+  place-items: center;
 }
 
-.form-footer a {
-  font-size: 16px;
-  font-weight: 500;
+.signup {
+  background-color: var(--grey-2);
+  min-height: 100vh;
+  padding-inline: 20px;
 }
 
-.form-footer a:hover {
-  background-color: #4e453783;
-  padding: 10px 7px 6px;
-  border-radius: 3px;
+.signup-box {
+  background: whitesmoke;
+  padding: 30px;
+  border-radius: 10px;
+  width: 60%;
+  margin-inline: auto;
+  position: relative;
+}
+
+.signup-box .fa-times {
+  font-size: 15px;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+}
+
+.signup-box .fa-times:hover {
+  color: var(--dim-blue);
+}
+
+.signup-box h4 {
+  color: var(--brown);
+}
+
+form .form-line {
+  display: flex;
+  margin: 40px 0;
+  gap: 20px;
+}
+
+.form-item {
+  position: relative;
+  height: 35px;
+  width: 100%;
+}
+
+.form-item input {
+  border-radius: 0px;
+  resize: none;
+  padding: 15px 0 10px;
+  font-size: 18px;
+  display: block;
+  background: transparent;
+  width: 100%;
+  height: 100%;
+  border: none;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.103);
+}
+
+.form-item label {
+  position: absolute;
+  pointer-events: none;
+  font-size: 17px;
+  bottom: 10px;
+}
+
+.form-item i {
+  position: absolute;
+  right: 0;
+  font-size: 17px;
+  cursor: pointer;
+  bottom: 10px;
+}
+
+.form-item input:focus {
+  outline: none;
+  border-bottom: 2px solid var(--brown);
+  transform-origin: center;
+  transition: all 0.4s ease-in-out;
+}
+
+.form-item input:focus + label,
+.form-item input:valid + label {
+  transform: translateY(-25px);
+  color: var(--brown);
+  font-size: 15px;
+  transition: all 0.4s ease;
+}
+
+.form-item input:valid + label {
+  color: initial;
 }
 
 .action-buttons {
@@ -109,11 +274,13 @@ export default {
   margin-bottom: 30px;
 }
 
-.action-buttons p {
+.action-buttons .or {
   display: grid;
   grid-template-columns: 1fr auto 1fr;
   place-content: center;
   align-items: center;
+  margin: 20px 0;
+  font-size: 14px;
   gap: 5px;
   width: 100%;
   text-align: center;
@@ -139,8 +306,26 @@ export default {
 }
 
 .gmail-btn:hover {
-  border-color: #4285f4;
-  background-color: white;
+  background-color: transparent;
+}
+
+.form-footer {
+  text-align: center;
+}
+
+.form-footer p {
+  margin-bottom: 10px;
+}
+
+.form-footer a {
+  font-size: 16px;
+  font-weight: 500;
+}
+
+.form-footer a:hover {
+  background-color: #4e453736;
+  padding: 10px 7px 6px;
+  border-radius: 3px;
 }
 
 .checkbox {
@@ -165,85 +350,44 @@ input[type="checkbox"] {
   cursor: pointer;
 }
 
-.grid-center {
-  display: grid;
-  place-items: center;
-}
-
-.signup {
-  background-color: var(--grey-2);
-  height: 100vh;
-}
-
-.signup-box {
-  background: whitesmoke;
-  padding: 30px;
-  border-radius: 10px;
-  min-width: 800px;
-  margin-inline: auto;
-}
-
-.signup-box h3 {
-  color: var(--brown);
-}
-
-form .form-line {
-  display: flex;
-  margin: 30px 0;
-  gap: 20px;
-}
-
-.form-item {
-  position: relative;
-  height: 35px;
-  width: 100%;
-}
-
-.form-item input {
-  border-radius: 0px;
-  resize: none;
-  padding: 15px 0 10px;
+/* Password Validation */
+.strength-validation {
   display: block;
-  background: transparent;
-  width: 100%;
-  height: 100%;
-  border: none;
-  border-bottom: 2px solid rgba(0, 0, 0, 0.103);
+  margin-top: 6px;
+  font-size: 13px;
 }
 
-.form-item input[type="tel"] {
-  width: 50%;
+.weak {
+  color: #ff0000;
 }
 
-.form-item label {
-  position: absolute;
-  pointer-events: none;
-  font-size: 17px;
-  bottom: 10px;
+.medium {
+  color: #ffc107;
 }
 
-.form-item i {
-  position: absolute;
-  right: 0;
-  font-size: 17px;
-  cursor: pointer;
-  bottom: 10px;
+.strong {
+  color: #4caf50;
 }
 
-.form-item input:focus {
-  outline: none;
-  border-bottom: 2px solid var(--brown);
-  transform-origin: center;
-  transition: all 0.4s ease-in-out;
-}
+/* Responsiveness */
 
-.form-item input:focus + label {
-  transform: translateY(-25px);
-  color: var(--brown);
-  font-size: 15px;
-  transition: all 0.4s ease;
+@media (max-width: 768px) {
+  .signup-box {
+    width: 100%;
+  }
 }
-.form-item input:valid + label {
-  color: initial;
+@media (max-width: 580px) {
+  form .form-line {
+    flex-direction: column;
+    gap: 30px;
+  }
+  .form-item input:focus + label,
+  .form-item input:valid + label {
+    transform: translateY(-20px);
+  }
+
+  .strength-validation {
+    text-align: right;
+  }
 }
 </style>
