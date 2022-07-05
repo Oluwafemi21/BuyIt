16b0069b788971da84fbf36af187066dc858267a
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-07-05 22:10:29 +0100

    grid display for shop fixed

--- src/components/home_components/products/FeaturedProducts.vue
+++ src/components/home_components/products/FeaturedProducts.vue
@@ -72,7 +72,7 @@ export default {
 
 .product__container {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     gap: 30px;
     padding-block: 4rem;
 }
