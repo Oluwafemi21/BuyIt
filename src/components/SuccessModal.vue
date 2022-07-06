481cb44b4a401067653850c358bf3fc03bd06a6d
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-07-06 12:40:38 +0100

    remove existing font-awesome icons

--- src/components/SuccessModal.vue
+++ src/components/SuccessModal.vue
@@ -12,7 +12,18 @@
                 <div class="modal-header">
                     <h3>Success</h3>
                     <button class="close" @click="$emit('close-modal')">
-                        <i class="fa fa-times"></i>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-x-lg"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
+                            />
+                        </svg>
                     </button>
                 </div>
                 <div class="modal-body">
@@ -115,10 +126,6 @@ export default {
 }
 .modal-footer button span {
     width: 100%;
-}
-.modal-footer button .icon {
-    position: absolute;
-    right: 20px;
 }
 
 .fade-enter-active,
--- src/views/UserRegistration/SignupBox.vue
+++ src/views/UserRegistration/SignupBox.vue
@@ -2,7 +2,18 @@
     <section class="signup grid-center">
         <div class="signup-box">
             <router-link to="/">
-                <i class="fas fa-times"></i>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-x-lg close-signup"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
+                    />
+                </svg>
             </router-link>
             <h4>Create Account</h4>
             <form @submit.prevent="registerUser">
@@ -31,16 +42,45 @@
                         />
 
                         <label>Password</label>
-                        <i
+                        <svg
+                            @click="togglePassword"
                             v-if="showPassword"
-                            class="fas fa-eye"
-                            @click="togglePassword"
-                        ></i>
-                        <i
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-eye"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"
+                            />
+                            <path
+                                d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"
+                            />
+                        </svg>
+
+                        <svg
                             v-else
-                            class="fas fa-eye-slash"
                             @click="togglePassword"
-                        ></i>
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-eye-slash"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"
+                            />
+                            <path
+                                d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"
+                            />
+                            <path
+                                d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"
+                            />
+                        </svg>
+
                         <span
                             v-show="validator"
                             class="strength-validation"
@@ -59,16 +99,44 @@
                             @keyup="validatePassword2"
                         />
                         <label>Confirm Password</label>
-                        <i
+                        <svg
                             v-if="confirmPassword"
-                            class="fas fa-eye"
                             @click="toggleConfirmPassword"
-                        ></i>
-                        <i
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-eye"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"
+                            />
+                            <path
+                                d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"
+                            />
+                        </svg>
+                        <svg
+                            @click="toggleConfirmPassword"
                             v-else
-                            class="fas fa-eye-slash"
-                            @click="toggleConfirmPassword"
-                        ></i>
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-eye-slash"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"
+                            />
+                            <path
+                                d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"
+                            />
+                            <path
+                                d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"
+                            />
+                        </svg>
+
                         <span
                             class="strength-validation"
                             :class="validator2 ? 'strong' : 'weak'"
@@ -258,7 +326,7 @@ export default {
     position: relative;
 }
 
-.signup-box .fa-times {
+.signup-box .close-signup {
     font-size: 15px;
     position: absolute;
     right: 20px;
@@ -266,7 +334,7 @@ export default {
     cursor: pointer;
 }
 
-.signup-box .fa-times:hover {
+.signup-box .close-signup:hover {
     color: var(--dim-blue);
 }
 
@@ -306,7 +374,7 @@ form .form-line {
     bottom: 10px;
 }
 
-.form-item i {
+.form-item svg {
     position: absolute;
     right: 0;
     font-size: 17px;
